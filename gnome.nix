cc8ef493f5a1ad5428e6e2ce30af35d01fb71238
Elias <tootbrute@tutanota.com>
2024-11-05 15:48:50 +0800

    update

--- gnome.nix
+++ gnome.nix
@@ -2,8 +2,12 @@
 
 {
 
+  # Auto unlock gnome keyring
+  services.gnome.gnome-keyring.enable = true;
+  security.pam.services.sddm.enableGnomeKeyring = true;
+
   # Remove default packages from GNOME
-  environment.gnome.excludePackages = with pkgs.gnome; [
+  environment.gnome.excludePackages = with  pkgs; [
     baobab      # disk usage analyzer
     cheese      # photo booth
     eog         # image viewer
@@ -12,7 +16,7 @@
     simple-scan # document scanner
     totem       # video player
     yelp        # help viewer
-    #evince      # document viewer
+    evince      # document viewer
     file-roller # archive manager
     geary       # email client
     seahorse    # password manager
@@ -21,25 +25,20 @@
     gnome-maps
     gnome-music
     gnome-software
-    #pkgs.gnome-extension-manager
-    #gnome-shell-extensions
-    pkgs.gnome-photos
-    pkgs.gnome-connections
-    pkgs.snapshot
+    gnome-extension-manager
+    gnome-shell-extensions
+    gnome-photos
+    gnome-connections
+    snapshot
     gnome-logs
     gnome-system-monitor
     gnome-calculator
-    pkgs.gnome-tour
-    #gnome-calendar
-    #gnome-characters
-    #gnome-clocks
-    #gnome-font-viewer gnome-screenshot
-    #gnome-disk-utilitys
+    gnome-tour
   ];
 
   environment.systemPackages = with pkgs; [
     #Gnome tweak tools
-    gnome.gnome-tweaks
+    gnome-tweaks
     gnomeExtensions.appindicator
     gnomeExtensions.caffeine
     gnomeExtensions.gsconnect
